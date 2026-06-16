00831de9c3edbff41c4c33322af2806cce1d2bc0
Naila Hafizha <naila241@mhs.usk.ac.id>
2026-06-16 13:25:58 +0700

    Create locationRoutes.js

---
+++ backend/routes/locationRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const locationController = require("../controllers/locationController");
+
+router.get("/", locationController.getLocations);
+router.get("/:id", locationController.getLocationById);
+router.post("/", locationController.createLocation);
+router.put("/:id", locationController.updateLocation);
+router.delete("/:id", locationController.deleteLocation);
+
+module.exports = router;
